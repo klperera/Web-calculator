4d5bff7babbe8531ec0693f2c4122fd6c9c90002
klperera <klperera@users.noreply.github.com>
2024-08-12 05:36:14 +0530

    simple updates

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const display = document.getElementById("display");
+var items = [];
+
+function push(data){
+    items.push(data);
+}
 
 function addToDisplay(input){
     display.value += input;
@@ -7,11 +12,21 @@ function clrDisplay(){
     display.value = "";
 }
 function calculate() {
+    items.push(`${display.value} = ${eval(display.value)}`);
     try{
         display.value = eval(display.value);
+        
     }
     catch (error){
         display.value = "Error"
     }
+}
+function history(){
+    var his_tab = document.getElementsByClassName("his_tab");
+    console.log(items);
     
+    
+    his_tab.textcontent = items;
+    
+
 }

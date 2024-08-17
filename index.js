6060fc35af743fd81ba66e98a67caba36060c33f
klperera <klperera@users.noreply.github.com>
2024-08-17 20:41:24 +0530

    adding js key events

--- index.js
+++ index.js
@@ -35,3 +35,63 @@ his_btn.addEventListener("click", () =>{
     }
     his_tab.style.display = "block";
 })
+document.addEventListener("keypress", event => {
+    console.log(event.key);
+    switch (event.key) {
+        case "0":
+            addToDisplay('0')
+            break;
+        case "1":
+            addToDisplay('1')
+            break;
+        case "2":
+            addToDisplay('2')
+            break;
+        case "3":
+            addToDisplay('3')
+            break;
+        case "4":
+            addToDisplay('4')
+            break;
+        case "5":
+            addToDisplay('5')
+            break;
+        case "6":
+            addToDisplay('6')
+            break;
+        case "7":
+            addToDisplay('7')
+            break;
+        case "8":
+            addToDisplay('8')
+            break;
+        case "9":
+            addToDisplay('9')
+            break;
+        case "+":
+            addToDisplay('+')
+            break;
+        case "-":
+            addToDisplay('-')
+            break;
+        case "*":
+            addToDisplay('*')
+            break;
+        case "/":
+            addToDisplay('/')
+            break;
+        case "%":
+            addToDisplay('%')
+            break;
+        case ".":
+            addToDisplay('.')
+            break;
+        case "Enter":
+            calculate()
+            break;
+        default:
+            console.log("press only a number.");
+            
+            break;
+    }
+})
